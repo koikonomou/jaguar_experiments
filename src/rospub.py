bb57a009830a8e845fe20a19810326ad13187f27
Katerina Oikonomou <cathrine.m.oik@gmail.com>
2018-11-01 16:48:42 +0200

    Publishing to cmd_vel for certain time

---
+++ src/rospub.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+import rospy
+import roslib
+import time 
+import rosbag
+import genpy
+from std_msgs.msg import Int32 , String
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Point, Quaternion , Twist
+
+
+pub1 = None
+
+# Initialization
+def init():
+    # global pub
+    rospy.init_node('rospub')
+
+    pub1 = rospy.Publisher("/cmd_vel", Twist, queue_size=1)
+
+    test_msg = Twist()
+    test_msg.linear.x = 0.1
+    test_msg.linear.y = 0.0
+    test_msg.linear.z = 0.0
+    # rate = rospy.Rate(10)
+    # while not rospy.is_shutdown():
+    timeout = 10
+    timeout_start = time.time() 
+    while time.time() < timeout_start + timeout:
+        pub1.publish(test_msg)
+        # rate.sleep()
+    rospy.spin()
+
+if __name__ == '__main__':
+    init()
